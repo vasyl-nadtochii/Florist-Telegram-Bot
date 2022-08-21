2d088e17dc49bf968db9ad82fd824327726e7786
Vasyl Nadtochii <vasyanadtochiy146@gmail.com>
2022-08-21 15:55:38 +0300

    Fix

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 import os
-from secrets import choice
 
 def __start():
-    def request_choice():
-        try:
-            choice = int(input("Your choice: "))
-        except:
-            print("ERROR: Wrong input type, try again")
-            request_choice()
-
-        return choice
+    choice = None
 
     print("MAIN: Choose what to run from options below:")
     print("1 - Run Florist Bot")
     print("2 - Run Florist Bot Unit Tests")
 
-    option = request_choice()
+    try:
+        choice = int(input("Your choice: "))
+    except:
+        print("ERROR: Wrong input type, try again")
+        __start()
+        return
 
-    if option == 1:
+    if choice == 1:
         os.system('python bot.py')
-    elif option == 2:
+    elif choice == 2:
         os.system('python tests.py')
     else:
         print("Wrong option, try again")

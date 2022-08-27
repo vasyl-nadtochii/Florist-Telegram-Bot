2f7cecc2483d31922dd04f6b0160aa6d79d93aef
Vasyl Nadtochii <vasyanadtochiy146@gmail.com>
2022-08-27 11:51:23 +0300

    Implemented keyboards manager

---
+++ Florist/Resources/Keyboards/keyboards.py
@@ -0,0 +1,20 @@
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from Florist.Services.Localizables.localizables import LocalizableManager
+
+class KeyboardMananger:
+
+    def __init__(self):
+        self.localized_manager = LocalizableManager()
+
+    def get_start_keyboard(self):
+        sign_in_btn = KeyboardButton(self.localized_manager.get_localized_string("sign_in_btn_text"))
+        sign_up_btn = KeyboardButton(self.localized_manager.get_localized_string("sign_up_btn_text"))
+
+        return ReplyKeyboardMarkup(
+            resize_keyboard = True,
+            one_time_keyboard = True
+        ).add(
+            sign_in_btn
+        ).add(
+            sign_up_btn
+        )
--- bot.py
+++ bot.py
@@ -3,13 +3,13 @@ from email.message import Message
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
 
 import Config.config as config
 import Config.debug_helper as helper
 import logging
 
 from Florist.Services.Localizables.localizables import LocalizableManager
+from Florist.Resources.Keyboards.keyboards import KeyboardMananger
 
 logging.basicConfig (level = logging.INFO)
 
@@ -17,24 +17,13 @@ bot = Bot(token= config.TOKEN)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
 localized_manager = LocalizableManager()
+keyboard_manager = KeyboardMananger()
 
 @dp.message_handler(commands = ['start'])
 async def on_start(message: Message):
-    sign_in_btn = KeyboardButton(localized_manager.get_localized_string("sign_in_btn_text"))
-    sign_up_btn = KeyboardButton(localized_manager.get_localized_string("sign_up_btn_text"))
-
-    keyboard = ReplyKeyboardMarkup(
-        resize_keyboard = True,
-         one_time_keyboard = True
-    ).add(
-        sign_in_btn
-    ).add(
-        sign_up_btn
-    )
-
     await message.reply(
         localized_manager.get_localized_string(key = "intro_message"),
-         reply_markup = keyboard
+        reply_markup = keyboard_manager.get_start_keyboard()
     )
 
 # TODO: temporary - remove that later
